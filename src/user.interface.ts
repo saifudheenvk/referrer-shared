ea5530c0c8ac9d5f23a13a388c842678ee739b25
saifudheen <saifudheen@innovatise.com>
2025-09-15 13:23:56 +0530

    updated interfaces

--- src/user.interface.ts
+++ src/user.interface.ts
@@ -1,63 +1,60 @@
+import { Document, Types } from 'mongoose';
 
+export interface IPermission {
+  authority: string;
+}
+
+export interface IPermissionGroup {
+  name: string;
+  description: string;
+  permissions: Types.ObjectId[] | IPermission[];
+}
+
+export interface IUserRole {
+  name: string;
+  type: RoleType;
+  description: string;
+  permissionGroups: Types.ObjectId[] | IPermissionGroup[];
+}
 
 export interface IUser {
-    email: string;
-    password: string;
-    phoneNumber: string;
-    role: IUserRole;
-    createdAt: Date;
-    updatedAt: Date;
+  email: string;
+  password?: string;
+  phoneNumber: string;
+  role: Types.ObjectId | IUserRole; 
+  company: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IPermissionDocument extends IPermission, Document {}
+export interface IPermissionGroupDocument extends IPermissionGroup, Document {
+  permissions: IPermissionDocument[];
+}
+export interface IUserRoleDocument extends IUserRole, Document {
+  permissionGroups: IPermissionGroupDocument[];
+}
+export interface IUserDocument extends IUser, Document {
+  comparePassword(password: string): Promise<boolean>;
+}
+
+export enum RoleType {
+  SUPER_ADMIN = 'SUPER_ADMIN',
+  SALES_ADMIN = 'SALES_ADMIN',
+  CONTENT_MANAGER = 'CONTENT_MANAGER',
+  CONTENT_ADMIN = 'CONTENT_ADMIN',
 }
 
 
-export interface IUserRole {
-    name: string;
-    type: RoleType;
-    description: string;
-    permissionGroups: IPermissionGroup[];
-  }
+export interface IAuth {
+  email: string;
+  password?: string;
+}
 
-
-  export interface IPermissionGroup {
-    name: string;
-    description: string;
-    permissions: IPermission[];
-  }
-
-
-  export interface IPermission {
-    authority: string;
-  }
-
-  export enum RoleType {
-    SUPER_ADMIN,
-    SALES_ADMIN,
-    CONTENT_MANAGER,
-    CONTENT_ADMIN
-  }
-
-
-  export interface IUserDocument {
-    _id?: string;
-    company?: string;
-    email?: string;
-    password?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-    comparePassword(password: string, hashedPassword: string): Promise<boolean>;
-    hashPassword(password: string): Promise<string>;
-  }
-
-  export interface IAuth {
-    email?: string;
-    password?: string;
-  }
-  
-
-  export interface ICreatUserPayload {
-    [key: string]: string;
-    company: string;
-    email: string;
-    password: string;
-    country: string;
-  }
+export interface ICreatUserPayload {
+  company: string;
+  email: string;
+  password?: string;
+  country: string;
+  phoneNumber: string;
+}
